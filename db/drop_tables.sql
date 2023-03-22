334c7133c3819606e335c0d8cace793c96cab607
tmanti <tmanti17@gmail.com>
2023-03-22 17:00:34 -0400

    add sql

---
+++ db/drop_tables.sql
@@ -0,0 +1,1 @@
+DROP TABLE `HotelManagement`.`Address`, `HotelManagement`.`booked`, `HotelManagement`.`Booking`, `HotelManagement`.`Customer`, `HotelManagement`.`Department`, `HotelManagement`.`Employee`, `HotelManagement`.`Room`, `HotelManagement`.`RoomCard`, `HotelManagement`.`Service`, `HotelManagement`.`Shift`, `HotelManagement`.`Transaction`, `HotelManagement`.`used_services`;
